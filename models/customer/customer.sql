5d14a70c19cbda9cd14f4df133cf8621f6fc337e
syedfai97@gmail.com <syedfai97@gmail.com>
2023-11-11 14:19:10 +0000

    1st commit

--- models/customer/customer.sql
+++ models/customer/customer.sql
@@ -1,25 +1,27 @@
-{{ config(materialized='table') }}
+{{ config(materialized="table") }}
 
-with source_data as (
+with
+    source_data as (
+        select
+            cn.customer_id,
+            cn.customer_name_,
+            zone,
+            vendor_code,
+            vertical,
+            status,
+            created_at,
+            sum(qty_ordered) as quantity,
+            sum(grand_total) as grand_total,
+            sum(commission) as commission,
+            sum(delivery_fee) as delivery_fee,
+            sum(flgpo) as flgpo
+        from `DEMOBQ1.Customers`
+        left join
+            `DEMOBQ1.Customer_name` cn
+            on cn.customer_id = `DEMOBQ1.Customers`.customer_id
+        group by 1, 2, 3, 4, 5, 6, 7
 
- select 
- cn.Customer_ID,
- cn.customer_name_,
- zone,
- vendor_code,
- vertical,status,
- created_at,
- sum(qty_ordered) as quantity,
- sum(grand_total) as grand_total,
- sum(commission) as commission,
- sum(delivery_fee) as delivery_fee,
- sum(flgpo) as flgpo 
-from `DEMOBQ1.Customers` 
-left join `DEMOBQ1.Customer_name` cn
-on cn.Customer_ID = `DEMOBQ1.Customers`.Customer_ID
-group by 1,2,3,4,5,6, 7
-
-)
+    )
 
 select *
 from source_data
--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -11,9 +11,22 @@
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+ select 
+ cn.Customer_ID,
+ cn.customer_name_,
+ zone,
+ vendor_code,
+ vertical,status,
+ created_at,
+ sum(qty_ordered) as quantity,
+ sum(grand_total) as grand_total,
+ sum(commission) as commission,
+ sum(delivery_fee) as delivery_fee,
+ sum(flgpo) as flgpo 
+from `DEMOBQ1.Customers` 
+left join `DEMOBQ1.Customer_name` cn
+on cn.Customer_ID = `DEMOBQ1.Customers`.Customer_ID
+group by 1,2,3,4,5,6, 7
 
 )
 
